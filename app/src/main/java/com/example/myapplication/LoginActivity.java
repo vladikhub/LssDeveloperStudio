22990d5f83132b0f1acf99cde227f7b5f643eefc
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2024-03-21 08:56:33 +0700

    Добовления иконки Аккаунта

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -6,6 +6,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
